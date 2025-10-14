e5a4958b7a8634ba34c257406762b38bbe960f17
Maikel Linke <maikel@email.org.au>
2025-10-14 16:18:59 +1100

    Generate gem structure with bundler
    * Ran `bundle gem taler`.
    * Updated some metadata.

---
+++ lib/taler.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "taler/version"
+
+module Taler
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/taler_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+RSpec.describe Taler do
+  it "has a version number" do
+    expect(Taler::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    pending
+
+    expect(false).to eq(true)
+  end
+end
