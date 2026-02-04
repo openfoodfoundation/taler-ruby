85cc0659d792534e52e766459490614c0f9d8079
Maikel Linke <maikel@email.org.au>
2026-02-05 10:21:24 +1100

    Fix spec for Ruby 3.3

--- spec/taler/order_spec.rb
+++ spec/taler/order_spec.rb
@@ -18,8 +18,11 @@ RSpec.describe Taler::Order do
     prompt "Pay at: #{order.status_url}"
 
     expect(order.fetch("order_status")).to eq "paid"
-    expect(order.to_s)
-      .to eq "#<Taler::Order https://backend.demo.taler.net/instances/sandbox/orders/2026.029-02M2JNJ2TGA2P {\"order_status\" => \"paid\", \"refunded\" => false}>"
+
+    # The spaces are only included in Ruby >= 3.4.
+    # We can remove the question marks after dropping support for Ruby 3.3.
+    # Ruby 3.3 support officially ends in April 2027.
+    expect(order.to_s).to match(/{"order_status" ?=> ?"paid", ?"refunded" ?=> ?false}>/)
 
     order.refund(refund: "KUDOS:4", reason: "test")
 
@@ -27,7 +30,9 @@ RSpec.describe Taler::Order do
 
     expect { order.reload }.to change { order.fetch("refunded") }.to true
 
-    expect(order.inspect)
-      .to eq '#<Taler::Order https://backend.demo.taler.net/instances/sandbox/orders/2026.029-02M2JNJ2TGA2P {"order_status" => "paid", "deposit_total" => "KUDOS:0", "wired" => true, "refunded" => true, "refund_pending" => false, "refund_amount" => "KUDOS:4"}>'
+    expect(order.inspect).to match(/"order_status" ?=> ?"paid"/)
+    expect(order.inspect).to match(/"wired" ?=> ?true/)
+    expect(order.inspect).to match(/"refunded" ?=> ?true/)
+    expect(order.inspect).to match(/"refund_amount" ?=> ?"KUDOS:4"/)
   end
 end
